51dfdb0e32626555f915941699844346c417ca9f
Nahom <zerihunnahom@gmail.com>
2023-02-14 19:00:58 +0300

    Refactor code to add styling to the booklist

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 /* eslint no-unused-vars: 0 */
-let books = JSON.parse(localStorage.getItem("books")) || [];
-const booksList = document.querySelector(".books");
-const form = document.querySelector("form");
+/* eslint max-classes-per-file: ["error", 2] */
+
+const books = JSON.parse(localStorage.getItem('books')) || [];
+const booksList = document.querySelector('.books');
+const form = document.querySelector('form');
 const { title, author } = form.elements;
 
 function generateBooks(data) {
   data.forEach((item) => {
-    const book = document.createElement("div");
+    const title = item.title[0].toUpperCase() + item.title.slice(1);
+    const author = item.author[0].toUpperCase() + item.author.slice(1);
+    const book = document.createElement('div');
     book.innerHTML = `
-        <h3>${item.title}</h3>
-        <p>${item.author}</p>
+        <p> "${title}" by <strong>${author}</strong></p>
         <button type="button" onclick="lib.removeBooks(${item.id})">Remove</button>
-        <hr/>
         `;
     booksList.appendChild(book);
   });
@@ -27,23 +29,27 @@ class Book {
 
 class Library {
   books = books;
+
   addBooks(title, author) {
     const idRandom = Math.floor(Math.random() * 100000);
     const bookInfo = new Book(idRandom, title.value, author.value);
     this.books.push(bookInfo);
-    console.log(this.books)
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
+
   removeBooks(id) {
     this.books = this.books.filter((book) => book.id !== id);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     window.location.reload();
   }
 }
+const lib = new Library();
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   lib.addBooks(title, author);
 });
 
-const lib = new Library();
 generateBooks(books);
+if (books.length === 0) {
+  booksList.style.display = 'none';
+}
